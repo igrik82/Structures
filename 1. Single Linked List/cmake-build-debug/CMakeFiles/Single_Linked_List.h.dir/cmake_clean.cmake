4dede0c7947870fe3ab791afbfba89df8a0b11b8
Igor Smolyakov <igrik82@gmail.com>
2019-04-08 23:54:36 +0500

    Version 1

---
+++ 1. Single Linked List/cmake-build-debug/CMakeFiles/Single_Linked_List.h.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Single_Linked_List.h.dir/Single_Linked_List.c.o"
+  "CMakeFiles/Single_Linked_List.h.dir/Example.c.o"
+  "Single_Linked_List.h.pdb"
+  "Single_Linked_List.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Single_Linked_List.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
